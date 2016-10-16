162bf0f8b04366511ad69c21f8d96a3b4e823874
Batuhan Erol <batu.erol@gmail.com>
2016-10-15 23:55:17 -0400

    File handle for file by month option.

--- Timesheet Helper/ExcelOperator.cs
+++ Timesheet Helper/ExcelOperator.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace TimesheetHelper
 {
     class ExcelOperator
     {
+        private Excel.Application mApplication;
+        private Excel.Workbook mWorkbook;
+        private Excel.Worksheet mWorksheet;
+
+        private string mFileName;
+
         public ExcelOperator(string fileName)
         {
+            this.mFileName = fileName;
+            mApplication = new Excel.Application();
 
+            if (System.IO.File.Exists(fileName))
+            {
+                mWorkbook = mApplication.Workbooks.Open(fileName);
+                //TODO(batuhan): open a copy to work on.
+                //mApplication.NewWorkbook;
+            }
+            else
+            {
+                mWorkbook = mApplication.Workbooks.Add();
+            }
+            mWorksheet = mWorkbook.Sheets[1];
+        }
+
+
+        public void SaveChanges()
+        {
+
+        }
+
+        ~ExcelOperator()
+        {
+            this.mWorksheet?.SaveAs(mFileName);
         }
     }
 }
