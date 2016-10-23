ed7980d20bf5ea71c804c32cf93100a84ed9720a
Batuhan Erol <batu.erol@gmail.com>
2016-10-23 17:01:47 -0400

    DisplayName markup extension to display property names.

---
+++ Timesheet Helper/Extension/DisplayNameExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace TimesheetHelper.Extension
+{
+    class DisplayNameExtension: MarkupExtension
+    {
+        public Type Type { get; set; }
+        public string PropertyName { get; set; }
+        public DisplayNameExtension() { }
+        public DisplayNameExtension(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            // TODO(batuhan): Fail-check
+            var property = Type.GetProperty(PropertyName);
+            var attrs = property.GetCustomAttributes(typeof(DisplayNameAttribute), false);
+            return (attrs[0] as DisplayNameAttribute).DisplayName;
+        }
+    }
+}
